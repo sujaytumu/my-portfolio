838d044110e962a8026088222a390cb7372af2a9
Saichandana <CHANDANA_GITHUB_EMAIL>
2026-09-07 10:45:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<h2 align="center">
+  My Portfolio <br/>
+  <a href="" target="_blank"> sujay.dev </a>
+</h2>
+
+
+<br/>
+
+
+
+
+## Built With
+
+My personal portfolio <a href="" target="_blank"> sujay.dev </a> had my tech skills and projects .<br/>
+
+And the project was built using these technologies.
+
+- React.js
+- Node.js
+- Express.js
+- CSS3
+- VsCode
+- Vercel
+
+## Features
+
+**📖 Multi-Page Layout**
+
+**🎨 Styled with React-Bootstrap and Css with easy to customize colors**
+
+**📱 Fully Responsive**
+
+## Getting Started
+
+Clone down this repository. You will need `node.js` and `git` installed globally on your machine.
+
+## 🛠 Installation and Setup
+
+1. Installation: `npm install`
+
+2. In the project directory, you can run: `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The page will reload if you make edits.
+
+## Instructions for using this portfolio
+
+Open the project folder and Navigate to `/src/components/`. <br/>
+You will find all the components used and you can edit your information accordingly.
+
+
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import foodapp from "../../Assets/Projects/foodapp.jpg";
+import chatapp from "../../Assets/Projects/chatapp.jpg";
+import nomads from "../../Assets/Projects/nomads.jpeg";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={foodapp}
+              isBlog={false}
+              title="Multi Vendor Food App"
+              description="A marketplace where vendors register, add firms & products, while customers browse and order food items without login."
+              ghLink="https://github.com/sujaytumu/Yummy-Food-App"
+              demoLink="https://yummy-food-app-customer-frontend.vercel.app/"
+            />
+          </Col>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatapp}
+              isBlog={false}
+              title="Real Time Chat-App"
+              description="A full-stack messaging platform enabling instant communication with online/offline status, theme toggle, and seamless chat experience."
+              ghLink="https://github.com/sujaytumu/Chat-App"
+              demoLink="https://chat-app-jjcg.onrender.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={nomads}
+              isBlog={false}
+              title="Nomads-Travel App"
+              description="A web application to manage travel itineraries, bookings, and user profiles for nomadic travelers."
+              ghLink="https://github.com/sujaytumu/nomads"
+            />
+          </Col>
+      
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
