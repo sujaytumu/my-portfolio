ce6d28ed1b71c12342d0b417b8efbf29b25cf25d
sujay <150987692+sujaytumu@users.noreply.github.com>
2026-09-09 11:16:03 +0530

    Comment out Postman icon in Toolstack
    Commented out the Postman icon in the Toolstack component.

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -23,9 +23,9 @@ function Toolstack() {
       </Col>
 
       {/* Postman */}
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
+      // <Col xs={4} md={2} className="tech-icons">
+      //   <SiPostman />
+      // </Col>
 
       {/* IntelliJ IDEA */}
       <Col xs={4} md={2} className="tech-icons">
