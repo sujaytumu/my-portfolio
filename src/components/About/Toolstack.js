5cd870a38d174e6034ab61611925c15433ecb7d1
sujay <150987692+sujaytumu@users.noreply.github.com>
2026-09-09 11:08:34 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,17 +1,57 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiPostman, SiVercel } from "react-icons/si";
+
+import { FaGithub } from "react-icons/fa";
+import { VscVscode } from "react-icons/vsc";
+
+import {
+  SiPostman,
+  SiIntellijidea,
+  SiEclipseide,
+  SiRailway,
+  SiRender,
+} from "react-icons/si";
+
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      
+
+      {/* GitHub */}
+      <Col xs={4} md={2} className="tech-icons">
+        <FaGithub />
+      </Col>
+
+      {/* Postman */}
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
+
+      {/* IntelliJ IDEA */}
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiIntellijidea />
       </Col>
+
+      {/* Eclipse */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiEclipseide />
+      </Col>
+
+      {/* VS Code */}
+      <Col xs={4} md={2} className="tech-icons">
+        <VscVscode />
+      </Col>
+
+      {/* Railway */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRailway />
+      </Col>
+
+      {/* Render */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRender />
+      </Col>
+
     </Row>
   );
 }
