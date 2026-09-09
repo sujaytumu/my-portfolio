e8b20987229453e185cdc4f95dd49a7a9c78b275
sujay <150987692+sujaytumu@users.noreply.github.com>
2026-09-09 11:09:14 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,42 +1,85 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+
 import { CgCPlusPlus } from "react-icons/cg";
-import { DiJavascript1, DiReact, DiNodejs, DiMongodb, DiPython, DiGit} from "react-icons/di";
-import { SiC, SiMysql, SiExpress } from "react-icons/si";
+
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiJava,
+  DiPostgresql,
+} from "react-icons/di";
+
+import {
+  //SiC,
+  SiMysql,
+  SiExpress,
+  SiSpringboot,
+} from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+
+      {/* C */}
+      {/* <Col xs={4} md={2} className="tech-icons">
         <SiC />
+      </Col> */}
+
+      
+
+      {/* Java */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
       </Col>
+
+      {/* C++ */}
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+
+      {/* JavaScript */}
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
-      </Col>
+
+      {/* React */}
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
+
+      {/* Spring Boot */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpringboot />
+      </Col>
+
+      {/* Node.js */}
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
+
+      {/* Express.js */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+
+      {/* MongoDB */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+
+      {/* MySQL */}
       <Col xs={4} md={2} className="tech-icons">
         <SiMysql />
       </Col>
+
+      {/* PostgreSQL */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+      </Col>
+
     </Row>
   );
 }
